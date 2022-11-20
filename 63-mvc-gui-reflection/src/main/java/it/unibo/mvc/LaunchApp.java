ed2d63cce8a063e78ae59d2eabd4b208196f8a31
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2022-11-20 18:15:26 +0100

    Es63 finito

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,6 +1,10 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 //import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberCollectionController;
 //import it.unibo.mvc.controller.DrawNumberControllerImpl;
@@ -13,12 +17,15 @@ import it.unibo.mvc.view.DrawNumberSwingView;
  */
 public final class LaunchApp {
 
+    final private static int NUM = 3;
+
     private LaunchApp() { }
 
     /**
      * Runs the application.
      *
      * @param args ignored
+     * @throws SecurityException
      * @throws ClassNotFoundException if the fetches class does not exist
      * @throws NoSuchMethodException if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
@@ -26,11 +33,14 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberCollectionController(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberOutView());
+        final Class cSwing = new DrawNumberSwingView().getClass();
+        final Class cOut = new DrawNumberOutView().getClass();
+        for (int i = 0; i < LaunchApp.NUM; i++) {
+            app.addView((DrawNumberView)cSwing.getConstructor().newInstance());
+            app.addView((DrawNumberView)cOut.getConstructor().newInstance());
+        }
     }
 }
